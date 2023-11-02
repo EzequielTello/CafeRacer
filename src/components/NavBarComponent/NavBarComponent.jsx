9db28e77d16c92b6196b7004b1a8b08efb981c49
Tu Nombre <ezetello23@gmail.com>
2023-11-02 10:38:32 -0300

    dando estilos

--- src/components/NavBarComponent/NavBarComponent.jsx
+++ src/components/NavBarComponent/NavBarComponent.jsx
@@ -3,6 +3,7 @@ import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import NavDropdown from "react-bootstrap/NavDropdown";
 import CartWidgetComponent from "../CartWidgetComponent/CartWidgetComponent";
+import "./NavBar.css";
 
 const NavBarComponent = () => {
   return (
